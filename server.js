bfe91338e9461c0bb17c8cf3927173dae07581ec
yazhini04 <anandhirt@gmail.com>
2017-08-17 13:47:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/article-one', function (req, res) {
+app.get('/article-one', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 }); 
 
-app.get('/ui/article-two', function(req, res){
+app.get('/article-two', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-app.get('/ui/article-three', function(req, res){
+app.get('/article-three', function(req, res){
    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
