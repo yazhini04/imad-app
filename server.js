45e1d41c49b51b5655f85f9bcaa7f18b8bf934c1
yazhini04 <anandhirt@gmail.com>
2017-08-17 13:31:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/article-two', function(req, res){
 });
 
 app.get('/article-three', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.hhtml'));
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.hhtml
 });
 
 app.get('/ui/style.css', function (req, res) {
