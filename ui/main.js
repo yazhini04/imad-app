1a5844e88dd7a3242536cbcf7ce39c7fa989ce09
yazhini04 <anandhirt@gmail.com>
2017-08-18 06:50:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 //Move the image
-var img = document.getElementbyId('madi');
+var img = document.getElementById('madi');
 img.onClick = function () {
   img.style.marginLeft = '100px';  
 };
