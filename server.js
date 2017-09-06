c615bf5d48337299b86ec5476f6aa6e1a8812985
yazhini04 <anandhirt@gmail.com>
2017-09-06 16:15:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,6 +117,22 @@ app.get('/test-db', function (req, res) {
  });
 });
 
+var counter = 0;
+app.get('/counter', function(req, res) {
+   counter = counter + 1;
+   res.send(counter.toString());
+});
+
+var names = [];
+app.get('/submit-name', function(req, res) { // /submit-name?name=xxxx
+    // get the name from the request
+    var name = req.query.name;
+    
+    names.push(name);
+    // JSON: Javascript Object Notation
+    res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName', function (req, res) {
     //articleName == article-one
     //articles[articleName] == content object for article
